acc7a1c0f0d0f20eb1b5e9df95d632e00b3d1d81
Avishaan <avishaan@codeHatcher.com>
2014-11-13 22:16:02 -0800

    basic interaction between components

--- chromeExt/script.js
+++ chromeExt/script.js
@@ -1,8 +1,18 @@
 var types = new Array("textarea","input","div","h1","h2","h3","span","p");
 var fontin ="Verdana";
 var fontout = "\'Courier New\'";
+console.log("exec script.js");
 
 (function(){
+  chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
+
   chrome.extension.sendRequest({
     set:"font"
   },function(response){
